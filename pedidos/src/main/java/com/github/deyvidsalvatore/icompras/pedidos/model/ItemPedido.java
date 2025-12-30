f62324154ed8e9678f3a7c60fa1385680f1858ca
Deyvid Santos <deyvidsantosdatascience@gmail.com>
2025-12-30 17:27:20 -0300

    feat(pedidos): Pedido and ItemPedido model

---
+++ pedidos/src/main/java/com/github/deyvidsalvatore/icompras/pedidos/model/ItemPedido.java
@@ -0,0 +1,38 @@
+package com.github.deyvidsalvatore.icompras.pedidos.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "item_pedido")
+@Getter
+@Setter
+@NoArgsConstructor
+public class ItemPedido implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long codigo;
+
+    @Column(name = "codigo_pedido")
+    private Long codigoPedido;
+
+    @Column(name = "codigo_produto")
+    private Long codigoProduto;
+
+    @Column(name = "quantidade")
+    private Integer quantidade;
+
+    @Column(name = "valor_unitario", precision = 16, scale = 2)
+    private BigDecimal valorUnitario;
+
+}
