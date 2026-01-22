f5ab99c1698f611c2f23010046c54dc61dcd9309
Deyvid Santos <deyvidsantosdatascience@gmail.com>
2026-01-21 21:35:51 -0300

    feat: Initial faturamentos project

---
+++ faturamentos/src/main/java/com/github/deyvidsalvatore/icompras/faturamentos/FaturamentosApplication.java
@@ -0,0 +1,13 @@
+package com.github.deyvidsalvatore.icompras.faturamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaturamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaturamentosApplication.class, args);
+	}
+
+}
---
+++ faturamentos/src/test/java/com/github/deyvidsalvatore/icompras/faturamentos/FaturamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.deyvidsalvatore.icompras.faturamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaturamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
